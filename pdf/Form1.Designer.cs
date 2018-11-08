d340002e6ac874b903a1a47c4b161e39980933a1
cccccmd <cmd13995606867@gmail.com>
2018-11-08 14:42:58 +0800

    1108-627-cmd

---
+++ pdf/Form1.Designer.cs
@@ -0,0 +1,274 @@
+﻿namespace pdf
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.序号 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.文件 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.类型 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Tag = "";
+            this.label1.Text = "输入目录：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "输出目录：";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle4;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.序号,
+            this.文件,
+            this.类型});
+            this.dataGridView1.Location = new System.Drawing.Point(21, 159);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(541, 218);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // 序号
+            // 
+            this.序号.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.序号.DefaultCellStyle = dataGridViewCellStyle5;
+            this.序号.FillWeight = 35F;
+            this.序号.HeaderText = "序号";
+            this.序号.Name = "序号";
+            // 
+            // 文件
+            // 
+            this.文件.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.文件.FillWeight = 215.2583F;
+            this.文件.HeaderText = "文件";
+            this.文件.MinimumWidth = 10;
+            this.文件.Name = "文件";
+            // 
+            // 类型
+            // 
+            this.类型.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.类型.DefaultCellStyle = dataGridViewCellStyle6;
+            this.类型.FillWeight = 39.05647F;
+            this.类型.HeaderText = "类型";
+            this.类型.MinimumWidth = 10;
+            this.类型.Name = "类型";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(102, 29);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(362, 21);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.Location = new System.Drawing.Point(102, 65);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(362, 21);
+            this.textBox2.TabIndex = 4;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(33, 111);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(71, 16);
+            this.radioButton1.TabIndex = 5;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "逐个输出";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(134, 111);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(131, 16);
+            this.radioButton2.TabIndex = 6;
+            this.radioButton2.Text = "合并成一个文件输出";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(487, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 21);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "选择";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(487, 65);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 21);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "选择";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.Location = new System.Drawing.Point(376, 415);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 30);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "转换";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button4.Location = new System.Drawing.Point(487, 415);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 30);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "取消";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(21, 388);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(491, 21);
+            this.progressBar1.TabIndex = 11;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(513, 392);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 14);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "未开始";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 462);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dataGridView1);
+            this.MinimumSize = new System.Drawing.Size(600, 500);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn 序号;
+        private System.Windows.Forms.DataGridViewTextBoxColumn 文件;
+        private System.Windows.Forms.DataGridViewTextBoxColumn 类型;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ pdf/Form1.cs
@@ -0,0 +1,297 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using Word = Microsoft.Office.Interop.Word;
+using Excel = Microsoft.Office.Interop.Excel;
+//using Microsoft.Office.Core;
+//using PdfSharp.Pdf.IO;
+//using PdfSharp.Pdf;
+//using PdfSharp.Drawing;
+using Document = iTextSharp.text.Document;
+using iTextSharp.text.pdf;
+using iTextSharp.text;
+
+namespace pdf
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private static List<FileInfo> lst = new List<FileInfo>();
+        private String inputPath;
+        private String outputPath;
+
+        private static void getdir(string path, List<FileInfo> list)
+        {
+
+            string[] dir = Directory.GetDirectories(path); //文件夹列表   
+            DirectoryInfo fdir = new DirectoryInfo(path);
+            FileInfo[] file = fdir.GetFiles();
+            if (file.Length != 0 || dir.Length != 0) //当前目录文件或文件夹不为空                   
+            {
+                foreach (FileInfo f in file) //显示当前目录所有文件 
+                    list.Add(f);
+                foreach (string d in dir)
+                {
+                    getdir(d, list);//递归   
+                }
+            }
+        }
+
+        private bool WordToPDF(string inePath, string outputPath)
+        {
+            bool result = false;
+            Microsoft.Office.Interop.Word.Application application = new Microsoft.Office.Interop.Word.Application();
+            Word.Document document = null;
+            try
+            {
+                application.Visible = false;
+                document = application.Documents.Open(inePath);
+                document.ExportAsFixedFormat(outputPath, Word.WdExportFormat.wdExportFormatPDF);
+                result = true;
+                
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                result = false;
+            }
+            finally
+            {
+                document.Close();
+                application.Quit();
+            }
+            return result;
+        }
+
+        private bool ExcelToPDF(string sourcePath, string targetPath)
+        {
+            bool result;
+            object missing = Type.Missing;
+            Excel.ApplicationClass application = null;
+            Excel.Workbook workBook = null;
+            try
+            {
+                application = new Excel.ApplicationClass();
+                object target = targetPath;
+                object type = Excel.XlFixedFormatType.xlTypePDF;
+                workBook = application.Workbooks.Open(sourcePath, missing, missing, missing, missing, missing,
+                        missing, missing, missing, missing, missing, missing, missing, missing, missing);
+
+                workBook.ExportAsFixedFormat(Excel.XlFixedFormatType.xlTypePDF, target, Excel.XlFixedFormatQuality.xlQualityStandard, true, false, missing, missing, missing, missing);
+                result = true;
+            }
+            catch
+            {
+                result = false;
+            }
+            finally
+            {
+                    workBook.Close(true, missing, missing);
+                    application.Quit();
+            }
+            return result;
+        }
+
+        //private void comPDF(String filename1, String filename2,String out3)
+        //{
+        //    PdfDocument inputDocument1 = PdfReader.Open(filename1, PdfDocumentOpenMode.Import);
+        //    PdfDocument inputDocument2 = PdfReader.Open(filename2, PdfDocumentOpenMode.Import);
+        //    PdfDocument outputDocument = new PdfDocument();
+        //    // Show consecutive pages facing. Requires Acrobat 5 or higher.
+        //    outputDocument.PageLayout = PdfPageLayout.TwoColumnLeft;
+        //    XFont font = new XFont("Verdana", 10, XFontStyle.Bold);
+        //    XStringFormat format = new XStringFormat();
+        //    format.Alignment = XStringAlignment.Center;
+        //    format.LineAlignment = XLineAlignment.Far;
+        //    //XGraphics gfx;
+        //    //XRect box;
+        //    int count = Math.Max(inputDocument1.PageCount, inputDocument2.PageCount);
+        //    for (int idx = 0; idx < count; idx++)
+        //    {
+        //        PdfPage page1 = inputDocument1.PageCount > idx ?
+        //          inputDocument1.Pages[idx] : new PdfPage();
+        //        PdfPage page2 = inputDocument2.PageCount > idx ?
+        //          inputDocument2.Pages[idx] : new PdfPage();
+
+        //        // Add both pages to the output document
+        //        page1 = outputDocument.AddPage(page1);
+        //        page2 = outputDocument.AddPage(page2);
+
+        //        // Write document file name and page number on each page
+        //        //gfx = XGraphics.FromPdfPage(page1);
+        //        //box = page1.MediaBox.ToXRect();
+        //        //box.Inflate(0, -10);
+        //        //gfx.DrawString(String.Format("{0} • {1}", filename1, idx + 1), font, XBrushes.Red, box, format);
+        //        //gfx = XGraphics.FromPdfPage(page2);
+        //        //box = page2.MediaBox.ToXRect();
+        //        //box.Inflate(0, -10);
+        //        //gfx.DrawString(String.Format("{0} • {1}", filename2, idx + 1), font, XBrushes.Red, box, format);
+        //    }
+        //    string filename = out3;
+        //    outputDocument.Save(filename);
+        //}
+
+        private void comPDF(List<string> filelist, string path0)
+        {
+            PdfReader reader;
+            List<PdfReader> readerList = new List<PdfReader>();
+            Document document = new Document();
+            PdfWriter writer = PdfWriter.GetInstance(document, new FileStream(path0, FileMode.Create));
+            document.Open();
+            PdfContentByte cb = writer.DirectContent;
+            PdfImportedPage newPage;
+            for (int i = 0; i < filelist.Count; i++)
+            {
+                reader = new PdfReader(filelist[i]);
+                int iPageNum = reader.NumberOfPages;
+                for (int j = 1; j <= iPageNum; j++)
+                {
+                    document.NewPage();
+                    newPage = writer.GetImportedPage(reader, j);
+                    cb.AddTemplate(newPage, 0, 0);
+                }
+                readerList.Add(reader);
+            }
+            document.Close();
+            foreach (var rd in readerList)//清理占用
+            {
+                rd.Dispose();
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lst.Clear();
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.Description = "请选择文件路径";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                inputPath = dialog.SelectedPath;
+                textBox1.Text = inputPath;
+                getdir(inputPath, lst);
+                foreach (FileInfo f in lst)
+                {
+                    String type = "其他文档";
+                    if (f.Extension == ".docx")
+                        type = "Word文档";
+                    if (f.Extension == ".xlsx")
+                        type = "Execel文档";
+                    if (f.Extension == ".txt")
+                        type = "文本文档";
+                    int index = this.dataGridView1.Rows.Add();
+                    this.dataGridView1.Rows[index].Cells[0].Value = index + 1;
+                    this.dataGridView1.Rows[index].Cells[1].Value = f.Name;
+                    this.dataGridView1.Rows[index].Cells[2].Value = type;
+                }
+                progressBar1.Value = 0;
+                progressBar1.Minimum = 0;
+                progressBar1.Maximum = lst.Count;
+                label3.Text = "已载入";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.Description = "请选择文件路径";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                outputPath = dialog.SelectedPath;
+                textBox2.Text = outputPath;
+                int i = outputPath.Length;
+                if (outputPath.Length == 3)
+                    outputPath = outputPath.Remove(2);
+            }
+        }
+
+        private String toPDF(FileInfo f, String s, int flag)
+        {
+            String newPath = outputPath;
+            String newName = f.Name;
+            newName = newName.Replace(s, "pdf");
+            if (flag == 1)
+            {
+                newPath = f.DirectoryName;
+                newPath = newPath.Replace(inputPath, outputPath);
+                if (!Directory.Exists(newPath))
+                    Directory.CreateDirectory(newPath);
+                newPath = newPath + "\\" + newName;
+            }
+            else
+            {
+                newPath = newPath + "\\temp\\";
+                Directory.CreateDirectory(newPath);
+                newPath = newPath + newName;
+            }
+            return newPath;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int flag = 1;
+            if (radioButton2.Checked)
+            {
+                flag = 0;
+                progressBar1.Maximum++;
+            }
+            foreach (FileInfo f in lst)
+            {
+                if (f.Extension == ".docx")
+                    WordToPDF(f.FullName, toPDF(f, "docx", flag));
+                if (f.Extension == ".txt")
+                    WordToPDF(f.FullName, toPDF(f, "txt", flag));
+                if (f.Extension == ".xlsx")
+                    ExcelToPDF(f.FullName, toPDF(f, "xlsx", flag));
+                progressBar1.Value++;
+                label3.Text = progressBar1.Value + "/" + lst.Count;
+            }
+            if (radioButton2.Checked)
+            {
+                List<FileInfo> l = new List<FileInfo>();
+                List<String> pdf = new List<String>();
+                DirectoryInfo dir = new DirectoryInfo(outputPath);
+                getdir(outputPath + "\\temp", l);
+                foreach (FileInfo f in l)
+                    pdf.Add(f.FullName);
+                string na = dir.Name;
+                label3.Text = "正在合并";
+                comPDF(pdf,outputPath+"\\"+dir.Name+".pdf");
+
+                //String path1 = "", path2, outputpath_t, temp;
+                //outputpath_t = outputPath + "\\1.pdf";
+                //temp = outputPath + "\\temp.pdf";
+                //foreach (FileInfo f in l)
+                //{
+                //    if (l.IndexOf(f) == 0)
+                //    {
+                //        path1 = f.FullName;
+                //        continue;
+                //    }
+                //    path2 = f.FullName;
+                //    if (File.Exists(outputpath_t))
+                //        File.Delete(outputpath_t);
+                //    //comPDF(path1, path2, outputpath_t);
+                //    FileInfo tempf = new FileInfo(outputpath_t);
+                //    if (File.Exists(temp))
+                //        File.Delete(temp);
+                //    tempf.CopyTo(temp);
+                //    path1 = temp;
+                //}
+                label3.Text = "正在清理临时文件";
+                foreach (FileInfo f in l)
+                    f.Delete();
+                Directory.Delete(outputPath + "\\temp");
+                progressBar1.Value++;
+            }
+            
+            label3.Text = "已完成";
+        }
+    }
+}
